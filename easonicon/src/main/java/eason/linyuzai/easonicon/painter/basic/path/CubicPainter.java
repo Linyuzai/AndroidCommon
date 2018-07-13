4f5085ff85d94c1d247159829151bdf1606f9d0d
linyuzai <120718461@qq.com>
2018-07-13 16:54:21 +0800

    modify some PathPainter

--- easonicon/src/main/java/eason/linyuzai/easonicon/painter/basic/path/CubicPainter.java
+++ easonicon/src/main/java/eason/linyuzai/easonicon/painter/basic/path/CubicPainter.java
@@ -7,24 +7,57 @@ import android.graphics.RectF;
 
 public abstract class CubicPainter extends PathPainter {
 
+    private PointF startPoint;
+    private PointF endPoint;
+    private PointF[] controlPoint = new PointF[2];
+
+    public CubicPainter() {
+        this(new PointF(), new PointF());
+    }
+
+    public CubicPainter(PointF controlPoint1, PointF controlPoint2) {
+        this(new PointF(), new PointF(), controlPoint1, controlPoint2);
+    }
+
+    public CubicPainter(PointF startPoint, PointF endPoint, PointF controlPoint1, PointF controlPoint2) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.controlPoint[0] = controlPoint1;
+        this.controlPoint[1] = controlPoint2;
+    }
+
     @Override
     public void configurePath(Path path, RectF draw, RectF original, Paint paint) {
-        PointF startPoint = getCoordinate(getStartPoint(draw, paint), draw);
-        PointF endPoint = getCoordinate(getEndPoint(draw, paint), draw);
-        PointF[] controlPoint = getControlPoints(draw, paint);
-        controlPoint[0] = getCoordinate(controlPoint[0], draw);
-        controlPoint[1] = getCoordinate(controlPoint[1], draw);
+        PointF startPoint = getCoordinate(this.startPoint, draw);
+        PointF endPoint = getCoordinate(this.endPoint, draw);
+        controlPoint[0] = getCoordinate(this.controlPoint[0], draw);
+        controlPoint[1] = getCoordinate(this.controlPoint[1], draw);
         path.moveTo(startPoint.x, startPoint.y);
         path.cubicTo(controlPoint[0].x, controlPoint[0].y, controlPoint[1].x, controlPoint[1].y, endPoint.x, endPoint.y);
     }
 
-    public PointF getStartPoint(RectF rectF, Paint paint) {
-        return new PointF(0f, 0f);
+    public PointF getStartPoint() {
+        return startPoint;
     }
 
-    public PointF getEndPoint(RectF rectF, Paint paint) {
-        return new PointF(rectF.width(), rectF.height());
+    public void setStartPoint(PointF startPoint) {
+        this.startPoint = startPoint;
     }
 
-    public abstract PointF[] getControlPoints(RectF rectF, Paint paint);
+    public PointF getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(PointF endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public PointF[] getControlPoint() {
+        return controlPoint;
+    }
+
+    public void setControlPoint(PointF controlPoint1, PointF controlPoint2) {
+        this.controlPoint[0] = controlPoint1;
+        this.controlPoint[1] = controlPoint2;
+    }
 }
--- easonicon/src/main/java/eason/linyuzai/easonicon/painter/basic/path/QuadPainter.java
+++ easonicon/src/main/java/eason/linyuzai/easonicon/painter/basic/path/QuadPainter.java
@@ -7,22 +7,54 @@ import android.graphics.RectF;
 
 public abstract class QuadPainter extends PathPainter {
 
+    private PointF startPoint;
+    private PointF endPoint;
+    private PointF controlPoint;
+
+    public QuadPainter() {
+        this(new PointF());
+    }
+
+    public QuadPainter(PointF controlPoint) {
+        this(new PointF(), new PointF(), controlPoint);
+    }
+
+    public QuadPainter(PointF startPoint, PointF endPoint, PointF controlPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.controlPoint = controlPoint;
+    }
+
     @Override
     public void configurePath(Path path, RectF draw, RectF original, Paint paint) {
-        PointF startPoint = getCoordinate(getStartPoint(draw, paint), draw);
-        PointF endPoint = getCoordinate(getEndPoint(draw, paint), draw);
-        PointF controlPoint = getCoordinate(getControlPoint(draw, paint), draw);
+        PointF startPoint = getCoordinate(this.startPoint, draw);
+        PointF endPoint = getCoordinate(this.endPoint, draw);
+        PointF controlPoint = getCoordinate(this.controlPoint, draw);
         path.moveTo(startPoint.x, startPoint.y);
         path.quadTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y);
     }
 
-    public PointF getStartPoint(RectF rectF, Paint paint) {
-        return new PointF(0f, 0f);
+    public PointF getStartPoint() {
+        return startPoint;
     }
 
-    public PointF getEndPoint(RectF rectF, Paint paint) {
-        return new PointF(rectF.width(), rectF.height());
+    public void setStartPoint(PointF startPoint) {
+        this.startPoint = startPoint;
     }
 
-    public abstract PointF getControlPoint(RectF rectF, Paint paint);
+    public PointF getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(PointF endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public PointF getControlPoint() {
+        return controlPoint;
+    }
+
+    public void setControlPoint(PointF controlPoint) {
+        this.controlPoint = controlPoint;
+    }
 }
